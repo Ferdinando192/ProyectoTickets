4c31d06d2d2350630e0e7a23c79e7c24898eaf94
Ferdinando <fsantamaria192@gmail.com>
2021-11-21 20:53:26 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoTickets/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoTickets.MvcApplication" Language="C#" %>
---
+++ ProyectoTickets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoTickets.Startup))]
+namespace ProyectoTickets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
