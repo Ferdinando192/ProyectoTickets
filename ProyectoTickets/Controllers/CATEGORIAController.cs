792b825393446bcddd50b663a504f237c24a4a48
AndreyLM10 <andre96cr@gmail.com>
2021-11-25 20:47:46 -0600

    Cambios Rol y Categorías

---
+++ ProyectoTickets/Controllers/CATEGORIAController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoTickets;
+
+namespace ProyectoTickets.Controllers
+{
+    public class CATEGORIAController : Controller
+    {
+        private db_a7cc1a_ticketsdbEntities1 db = new db_a7cc1a_ticketsdbEntities1();
+
+        // GET: CATEGORIA
+        public ActionResult Index()
+        {
+            return View(db.CATEGORIA.ToList());
+        }
+
+        // GET: CATEGORIA/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORIA cATEGORIA = db.CATEGORIA.Find(id);
+            if (cATEGORIA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORIA);
+        }
+
+        // GET: CATEGORIA/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CATEGORIA/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_CATEGORIA,NOMBRE_CATEGORIA")] CATEGORIA cATEGORIA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CATEGORIA.Add(cATEGORIA);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cATEGORIA);
+        }
+
+        // GET: CATEGORIA/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORIA cATEGORIA = db.CATEGORIA.Find(id);
+            if (cATEGORIA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORIA);
+        }
+
+        // POST: CATEGORIA/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_CATEGORIA,NOMBRE_CATEGORIA")] CATEGORIA cATEGORIA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cATEGORIA).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cATEGORIA);
+        }
+
+        // GET: CATEGORIA/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CATEGORIA cATEGORIA = db.CATEGORIA.Find(id);
+            if (cATEGORIA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cATEGORIA);
+        }
+
+        // POST: CATEGORIA/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CATEGORIA cATEGORIA = db.CATEGORIA.Find(id);
+            db.CATEGORIA.Remove(cATEGORIA);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
