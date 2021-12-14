ea03c2be5cfa6420aa4b402f1ac24d67235e00cb
Ferdinando <fsantamaria192@gmail.com>
2021-12-13 20:18:15 -0600

    login-register modal

--- ProyectoTickets/Models/AccountViewModels.cs
+++ ProyectoTickets/Models/AccountViewModels.cs
@@ -68,6 +68,7 @@ namespace ProyectoTickets.Models
         [EmailAddress]
         [Display(Name = "Correo electrónico")]
         public string Email { get; set; }
+        
 
         [Required]
         [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 6)]
